dbcfbc4d4a06d3fc3556abebb3e35a1bc4958030
Gyeongwon Kim <kkwywy7963@gmail.com>
2019-11-02 00:30:03 -0400

    AddBtn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import Searchbar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
-
+import AddBtn from './components/layout/AddBtn';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -15,6 +15,7 @@ const App = () => {
     <Fragment>
       <Searchbar />
       <div className='container'>
+        <AddBtn />
         <Logs />
       </div>
     </Fragment>
