9a015d2c5bc457bdf8a52553b132979497115d8b
Gyeongwon Kim <kkwysy7963@gmail.com>
2019-11-06 22:52:57 -0500

    Update Logs

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from '../actions/types';
 
 const initialState = {
@@ -21,17 +24,34 @@ export default (state = initialState, action) => {
         logs: action.payload,
         loading: false
       };
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
+        loading: false
+      };
     case DELETE_LOG:
       return {
         ...state,
         logs: state.logs.filter(log => log.id !== action.payload),
         loading: false
       };
-    case ADD_LOG:
+    case UPDATE_LOG:
       return {
         ...state,
-        logs: [...state.logs, action.payload],
-        loading: false
+        logs: state.logs.map(log =>
+          log.id === action.payload.id ? action.payload : log
+        )
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
       };
     case SET_LOADING:
       return {
